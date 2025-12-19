ddf39d6df0777d574e526d89473f3a86880d5a44
Asiwaju-Adeniyi <makindeadeniyi1@gmail.com>
2025-12-19 20:39:19 +0100

    Update BigSecBig.cpp

--- ArrVec/BigSecBig.cpp
+++ ArrVec/BigSecBig.cpp
@@ -1,16 +1,17 @@
-#include <cstdlib>
 #include <iostream>
-#include <ctime>
-#include <iomanip>
-#include <fstream>
+#include <limits>
 
 void ArrayFunc(int arr[], int size) {
     for (int i = 0; i < size; i++) {
-        std::cout << "Enter element " << i + 1 << std::endl;
-        std::cin >> arr[i];
+        while (true) {
+            std::cout << "Enter element " << i + 1 << ": ";
+            std::cin >> arr[i];
 
-        if (!std::cin) {
-            std::cout << "Invalid input. Please enter an integer." << std::endl;
+            if (std::cin) {
+                break;
+            }
+
+            std::cout << "Invalid input. Please enter an integer.\n";
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
@@ -22,32 +23,31 @@ int main() {
     int arr[size];
     ArrayFunc(arr, size);
 
-    for (int value : arr) {
-        std::cout << value << " ";
-    }
+    int biggest = std::numeric_limits<int>::min();
+    int secondBiggest = std::numeric_limits<int>::min();
 
-    int biggest = 0;
-    int secondBiggest = 0;
 
     for (int i = 0; i < size; i++) {
-        if (arr[i] > biggest && arr[i] % 2 == 0) {
+        if (arr[i] % 2 == 0 && arr[i] > biggest) {
             biggest = arr[i];
         }
-}
-   std::cout << "Biggest: " << biggest << std::endl;
+    }
+
 
     for (int i = 0; i < size; i++) {
-        if (arr[i] <= biggest) {
+        if (arr[i] % 2 == 0 &&
+            arr[i] < biggest &&
+            arr[i] > secondBiggest) {
             secondBiggest = arr[i];
         }
-
     }
-    std::cout << "Second Biggest: " << secondBiggest << std::endl;
+
+    if (secondBiggest == std::numeric_limits<int>::min()) {
+        std::cout << "Not enough even numbers.\n";
+    } else {
+        std::cout << "Biggest even: " << biggest << std::endl;
+        std::cout << "Second biggest even: " << secondBiggest << std::endl;
+    }
 
     return 0;
-
 }
-
-
-
-    
