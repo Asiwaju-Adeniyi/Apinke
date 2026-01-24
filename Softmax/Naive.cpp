b10f240b0456a5639e5d6f0d922305abedaae30f
Asiwaju-Adeniyi <makindeadeniyi1@gmail.com>
2026-01-24 01:42:39 +0100

    Create Naive.cpp

---
+++ Softmax/Naive.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+#include <cmath> 
+#include <vector>
+
+std::vector<double> softmax (const std::vector<double>& x) {
+    int V = x.size();
+    std::vector<double> y(V);
+
+    double m = -std::numeric_limits<double>::infinity();
+
+    double d = 0.0f;
+
+    for (int j = 0; j < V; j++) {
+        d += std::exp(x[j]);
+    }
+
+    for (int i = 0; i < V; i++) {
+        y[i] = std::exp(x[i]) / d;
+    }
+
+    return y;
+}
